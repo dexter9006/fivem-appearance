385760c29aec227542344a8313b70c5ca255c941
dexter9006 <52006237+dexter9006@users.noreply.github.com>
2024-02-17 13:26:15 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -70,19 +70,7 @@ shared_scripts {
   "shared/framework/framework.lua",
   "shared/framework/esx/util.lua",
   "locales/locales.lua",
-  "locales/ar.lua",
-  "locales/bg.lua",
-  "locales/cs.lua",
-  "locales/de.lua",
-  "locales/en.lua",
-  "locales/es-ES.lua",
   "locales/fr.lua",
-  "locales/hu.lua",
-  "locales/it.lua",
-  "locales/nl.lua",
-  "locales/pt-BR.lua",
-  "locales/ro-RO.lua",
-  "locales/id.lua",
   "@ox_lib/init.lua"
 }
 
